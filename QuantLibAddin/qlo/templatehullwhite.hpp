0410b13766c832b6356f49560f48db953adea2cf
Sebastian Schlenkrich <sebastian.schlenkrich@d-fine.de>
2014-12-14 14:10:54 +0100

    set up and price fixed rate bond options

--- QuantLibAddin/qlo/templatehullwhite.hpp
+++ QuantLibAddin/qlo/templatehullwhite.hpp
@@ -13,21 +13,22 @@
 //#include <ql/experimental/template/auxilliaries/MinimADVariable2.hpp>
 
 #include <ql/experimental/template/hullwhite/hullwhitemodels.hpp>
+#include <ql/experimental/template/hullwhite/fixedratebondoption.hpp>
+#include <ql/experimental/template/hullwhite/bondoptionengine.hpp>
+#include <ql/instruments/bonds/fixedratebond.hpp>
+#include <ql/instruments/swaption.hpp>
 
 #include <qlo/baseinstruments.hpp>
+#include <qlo/termstructures.hpp>
+#include <qlo/pricingengines.hpp>
 
 /*
 #include <ql/experimental/templatehullwhite/adtageohullwhitemodel.hpp>
 #include <ql/experimental/templatehullwhite/swaptionproperties.hpp>
-#include <ql/experimental/templatehullwhite/fixedratebondoption.hpp>
-#include <ql/experimental/templatehullwhite/bondoptionengine.hpp>
-#include <ql/instruments/bonds/fixedratebond.hpp>
 #include <ql/indexes/iborindex.hpp>
 #include <ql/experimental/templatehullwhite/sabrswaptioncube.hpp>
 
 #include <qlo/vanillaswap.hpp>
-#include <qlo/termstructures.hpp>
-#include <qlo/pricingengines.hpp>
 #include <qlo/indexes/iborindex.hpp>
 */
 
@@ -63,7 +64,6 @@ namespace QuantLibAddin {
 							 bool permanent);
     };
 
-/*
 	class FixedRateBondOption : public Instrument {
 	public:
 		FixedRateBondOption( const boost::shared_ptr<ObjectHandler::ValueObject>& properties,
@@ -72,65 +72,12 @@ namespace QuantLibAddin {
 							 const std::vector<QuantLib::Real>& dirtyStrikeValues,
 						     const QuantLib::Option::Type callOrPut,
 							 bool permanent);
-        FixedRateBondOption ( const boost::shared_ptr<ObjectHandler::ValueObject>& properties,
-                              const boost::shared_ptr<QuantLib::SwaptionProperties>& swaptionProperties,
-                              bool permanent);
-	};
 
-    // helper container for reference swaption properties
-	// this is necessary since qlgensrc allows only a limited number of parametrs
-	class SwaptionProperties : public ObjectHandler::LibraryObject<QuantLib::SwaptionProperties> {
-	public:
-		SwaptionProperties(    const boost::shared_ptr<ObjectHandler::ValueObject>&    properties,
-							   //  fixed leg
-							   const QuantLib::Rate                                    fixedRate,
-							   const QuantLib::DayCounter&                             fixedDayCount,
-						       const QuantLib::Period&                                 fixedTenor,
-						       const QuantLib::Calendar&                               fixedCalendar,
-                               const QuantLib::BusinessDayConvention                   fixedConvention,
-						       //  floating leg
-						       const boost::shared_ptr<QuantLib::IborIndex>&           iborIndex,
-						       const QuantLib::DayCounter&                             floatDayCount,    // should be consistent to iborIndex
-						       const QuantLib::Period&                                 floatTenor,       // should be consistent to iborIndex
-						       const QuantLib::Calendar&                               floatCalendar,    // should be consistent to iborIndex
-                               const QuantLib::BusinessDayConvention                   floatConvention,  // should be consistent to iborIndex
-						       //  additional general properties
-						       const QuantLib::VanillaSwap::Type                       swapType,         // payer or receiver swap
-						       const QuantLib::Date                                    swapMaturity,     // we calibrate swaptions with equal swap maturities     
-						       const QuantLib::BusinessDayConvention                   paymentConvention,
-						       // swaption properties
-					           const std::vector<QuantLib::Date>&                      exerciseDates,    // option exercise dates should be consistent to bond option exercise dates
-					           const std::vector<QuantLib::Date>&                      settlementDates,  // effective (start) date of underlying swap, usually two BD after exercise
-						       const QuantLib::Settlement::Type                        cashOrPhysical,
-						       const QuantLib::Handle<QuantLib::YieldTermStructure>&   discountCurve,
-						       const QuantLib::Handle<QuantLib::SwaptionVolatilityStructure>&  swaptionVola,
-							   bool                                                    permanent);
-
-		SwaptionProperties(    const boost::shared_ptr<ObjectHandler::ValueObject>&    properties,
-                               //  fixed leg
-							   const QuantLib::Rate                                    fixedRate,
-							   const QuantLib::DayCounter&                             fixedDayCount,
-						       const QuantLib::Period&                                 fixedTenor,
-						       const QuantLib::Calendar&                               fixedCalendar,
-                               const QuantLib::BusinessDayConvention                   fixedConvention,
-						       //  floating leg
-						       const boost::shared_ptr<QuantLib::IborIndex>&           iborIndex,
-						       const QuantLib::DayCounter&                             floatDayCount,    // should be consistent to iborIndex
-						       const QuantLib::Period&                                 floatTenor,       // should be consistent to iborIndex
-						       const QuantLib::Calendar&                               floatCalendar,    // should be consistent to iborIndex
-                               const QuantLib::BusinessDayConvention                   floatConvention,  // should be consistent to iborIndex
-						       //  additional general properties
-						       const QuantLib::VanillaSwap::Type                       swapType,         // payer or receiver swap
-						       const QuantLib::Date                                    swapMaturity,     // we calibrate swaptions with equal swap maturities     
-						       const QuantLib::BusinessDayConvention                   paymentConvention,
-						       // swaption properties
-                               const boost::shared_ptr<QuantLib::Schedule>&            settlementSchedule,  // anker point are the settlent dates
-                               const QuantLib::Calendar&                               exerciseCalendar,
-					           const QuantLib::Period&                                 exerciseOffset,      // exercise date is exerciseDate + exerciseOffset (e.g. -2D)
-						       const QuantLib::Settlement::Type                        cashOrPhysical,
-						       const QuantLib::Handle<QuantLib::YieldTermStructure>&             discountCurve,
-						       const QuantLib::Handle<QuantLib::SwaptionVolatilityStructure>&    swaptionVola,
-                               bool                                                    permanent);
+		FixedRateBondOption( const boost::shared_ptr<ObjectHandler::ValueObject>&    properties,
+							 const boost::shared_ptr<QuantLib::Swaption>&            swaption,
+							 const QuantLib::Handle<QuantLib::YieldTermStructure>&   discountCurve,
+							 bool                                                    contTenorSpread,						
+						     bool                                                    permanent);
 	};
 
 	class BondOptionEngine : public PricingEngine {
@@ -142,32 +89,7 @@ namespace QuantLibAddin {
 						  const QuantLib::Real                                    gridRadius,
 						  const QuantLib::Real                                    bermudanTolerance,
 						  bool                                                    permanent);
-	    // constructor with given model and reference European prices
-		BondOptionEngine( const boost::shared_ptr<ObjectHandler::ValueObject>&    properties,
-						  const boost::shared_ptr<QuantLib::RealHullWhiteModel>&  model,
-						  const QuantLib::Size                                    dimension,
-						  const QuantLib::Real                                    gridRadius,
-						  const QuantLib::Real                                    bermudanTolerance,
-						  const std::vector<QuantLib::Real>&                      referenceEuropeanPrices,
-						  const QuantLib::Real                                    calibToleranceVola,
-						  bool                                                    permanent);
-
-	    // constructor with given model and reference Swap
-		BondOptionEngine( const boost::shared_ptr<ObjectHandler::ValueObject>&    properties,
-						  const boost::shared_ptr<QuantLib::RealHullWhiteModel>&  model,
-						  const QuantLib::Size                                    dimension,
-						  const QuantLib::Real                                    gridRadius,
-						  const QuantLib::Real                                    bermudanTolerance,
-						  //    underlying vanilla swaption properties
-						  const boost::shared_ptr<QuantLib::SwaptionProperties>&  swaptionProperties,
-						  const QuantLib::Real                                    calibToleranceVola,
-						  bool                                                    permanent);
 	};
-
-	typedef SwaptionProperties SwaptionPropertiesType;
-
-*/
-
 
 }
 
